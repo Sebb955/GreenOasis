0882cfe6fd771f130962f82f93c62d7c1938f05a
Sebb955 <slafrizi@gmail.com>
2023-08-05 18:26:47 +0200

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+// import { useState, useEffect } from 'react'
+import "../styles/Cart.css";
+
+
+function Cart({ cart, updateCart }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const total = cart.reduce(
+    (acc, plantType) => acc + plantType.amount * plantType.price,
+    0
+  );
+//   useEffect(() => {
+//     document.title = `LMJ: ${total}€ d'achats`
+// }, [total])
+  return isOpen ? (
+    <div className="lmj-cart">
+      <button
+        className="lmj-cart-toggle-button"
+        onClick={() => setIsOpen(false)}
+      >
+        Fermer
+      </button>
+      {cart.length > 0 ? (
+        <div>
+          <h2>Panier</h2>
+          <ul>
+            {cart.map(({ name, price, amount }, index) => (
+              <div key={`${name}-${index}`}>
+                {name} {price}€ x {amount}
+                <button onClick={() => removeFromCart(name)}>Supprimer</button>
+              </div>
+            ))}
+          </ul>
+          <h3>Total :{total}€</h3>
+          <button onClick={() => updateCart([])}>Vider le panier</button>
+        </div>
+      ) : (
+        <div>Votre panier est vide</div>
+      )}
+    </div>
+  ) : (
+    <div className="lmj-cart-closed">
+      <button
+        className="lmj-cart-toggle-button"
+        onClick={() => setIsOpen(true)}
+      >
+        Ouvrir le Panier
+      </button>
+    </div>
+  );
+  function removeFromCart(name) {
+    const updatedCart = cart
+      .map((plant) => {
+        if (plant.name === name) {
+          return {
+            ...plant,
+            amount: plant.amount - 1,
+          };
+        }
+        return plant;
+      })
+      .filter((plant) => plant.amount > 0);
+
+    updateCart(updatedCart);
+  }
+}
+
+export default Cart;
